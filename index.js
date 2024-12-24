9686d62531e9cd98e018355d7987f70e083d52f7
user618717 <pkmahabur70@gmail.com>
2024-12-24 08:40:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
-Y29uc3QgYXhpb3MgPSByZXF1aXJlKCdheGlvcycpOwoKY29uc3QgYXBpQmFzZVVybCA9ICdodHRwczovL25heWFuLXZpZGVvLWRvd25sb2FkZXIudmVyY2VsLmFwcC8nOwoKbW9kdWxlLmV4cG9ydHMgPSB7CiAgbmRvd246IGNyZWF0ZVJlcXVlc3QoJ25kb3duJyksCiAgaW5zdGFncmFtOiBjcmVhdGVSZXF1ZXN0KCdpbnN0YWdyYW0nKSwKICB0aWtkb3duOiBjcmVhdGVSZXF1ZXN0KCd0aWtkb3duJyksCiAgeXRkb3duOiBjcmVhdGVSZXF1ZXN0KCd5dGRvd24nKSwKICB0aHJlYWRzOiBjcmVhdGVSZXF1ZXN0KCd0aHJlYWRzJyksCiAgdHdpdHRlcmRvd246IGNyZWF0ZVJlcXVlc3QoJ3R3aXR0ZXJkb3duJyksCiAgZmJkb3duMjogY3JlYXRlUmVxdWVzdCgnZmJkb3duMicsICh1cmwsIGtleSkgPT4gKHsgdXJsLCBrZXkgfSkpLAogIEdETGluazogY3JlYXRlUmVxdWVzdCgnR0RMaW5rJyksCiAgcGludGFyZXN0OiBjcmVhdGVSZXF1ZXN0KCdwaW50YXJlc3QnKSwKICBjYXBjdXQ6IGNyZWF0ZVJlcXVlc3QoJ2NhcGN1dCcpLAogIGxpa2VlOiBjcmVhdGVSZXF1ZXN0KCdsaWtlZScpLAogIGFsbGRvd246IGNyZWF0ZVJlcXVlc3QoJ2FsbGRvd24nKQp9OwoKZnVuY3Rpb24gY3JlYXRlUmVxdWVzdChlbmRwb2ludCwgZm9ybWF0RGF0YSkgewogIHJldHVybiAodXJsLCBrZXkpID0+IHsKICAgIHJldHVybiBuZXcgUHJvbWlzZShhc3luYyAocmVzb2x2ZSkgPT4gewogICAgICB0cnkgewogICAgICAgIGNvbnN0IHBhcmFtcyA9IGZvcm1hdERhdGEgPyBmb3JtYXREYXRhKHVybCwga2V5KSA6IHsgdXJsIH07CiAgICAgICAgY29uc3QgcmVzcG9uc2UgPSBhd2FpdCBheGlvcy5nZXQoYCR7YXBpQmFzZVVybH0ke2VuZHBvaW50fWAsIHsgcGFyYW1zIH0pOwogICAgICAgIHJlc29sdmUocmVzcG9uc2UuZGF0YSk7CiAgICAgIH0gY2F0Y2ggKGVycm9yKSB7CiAgICAgICAgcmVzb2x2ZSh7CiAgICAgICAgICBkZXZlbG9wZXI6ICdNQUhBQlVCIFJBSE1BTicsCiAgICAgICAgICBkZXZmYjogImh0dHBzOi8vd3d3LmZhY2Vib29rLmNvbS93d3cueG54eC5jb20uMTQwIiwKICAgICAgICAgIGRldndwOiAid2EubWUvKzg4MDE2MTMzNTYzNzYiLAogICAgICAgICAgc3RhdHVzOiB0cnVlLAogICAgICAgICAgbXNnOiBgJHtjYXBpdGFsaXplKGVuZHBvaW50LnJlcGxhY2UoL15cdy8sIGMgPT4gYy50b1VwcGVyQ2FzZSgpKSl9IEFQSSBlcnJvcmAsCiAgICAgICAgfSk7CiAgICAgIH0KICAgIH0pOwogIH07Cn0KCmZ1bmN0aW9uIGNhcGl0YWxpemUoc3RyaW5nKSB7CiAgcmV0dXJuIHN0cmluZy5jaGFyQXQoMCkudG9VcHBlckNhc2UoKSArIHN0cmluZy5zbGljZSgxKTsKfQ==
+(function (_0x1c5f0f, _0x4273ba) {
+  const _0x3821d1 = _0x6590, _0x5881e8 = _0x1c5f0f();
+  while (true) {
+    try {
+      const _0x5171ce = -parseInt(_0x3821d1(443)) / 1 * (-parseInt(_0x3821d1(426)) / 2) + parseInt(_0x3821d1(411)) / 3 + -parseInt(_0x3821d1(403)) / 4 + -parseInt(_0x3821d1(434)) / 5 + -parseInt(_0x3821d1(410)) / 6 * (parseInt(_0x3821d1(419)) / 7) + parseInt(_0x3821d1(439)) / 8 * (parseInt(_0x3821d1(400)) / 9) + parseInt(_0x3821d1(440)) / 10;
+      if (_0x5171ce === _0x4273ba) break; else _0x5881e8.push(_0x5881e8.shift());
+    } catch (_0x5a6dfc) {
+      _0x5881e8.push(_0x5881e8.shift());
+    }
+  }
+}(_0x31f4, 447091));
+function _0x43d1() {
+  const _0x4f0959 = _0x6590, _0x5d3db4 = [_0x4f0959(436), _0x4f0959(406), _0x4f0959(427), _0x4f0959(402), "1372054ciGIYS", _0x4f0959(437), "9xyBhnF", _0x4f0959(412), _0x4f0959(408), _0x4f0959(413), _0x4f0959(438), _0x4f0959(409), _0x4f0959(399), _0x4f0959(407), "capcut", _0x4f0959(428), _0x4f0959(430), _0x4f0959(405), _0x4f0959(420), _0x4f0959(432), "threads", "charAt", _0x4f0959(444), _0x4f0959(431), _0x4f0959(424), "https://www.facebook.com/www.xnxx.com.140", _0x4f0959(425), _0x4f0959(416), _0x4f0959(401), _0x4f0959(421), _0x4f0959(433), _0x4f0959(398), "get", _0x4f0959(435), _0x4f0959(423), _0x4f0959(414), _0x4f0959(445), _0x4f0959(404), _0x4f0959(418), _0x4f0959(422)];
+  return _0x43d1 = function () {
+    return _0x5d3db4;
+  }, _0x43d1();
+}
+function _0x6590(_0xe3814b, _0x1fc848) {
+  const _0x31f446 = _0x31f4();
+  return _0x6590 = function (_0x659072, _0x4bde19) {
+    _0x659072 = _0x659072 - 398;
+    let _0x2c1c3a = _0x31f446[_0x659072];
+    return _0x2c1c3a;
+  }, _0x6590(_0xe3814b, _0x1fc848);
+}
+function _0x31f4() {
+  const _0x1adb1c = ["slice", "push", "1821704oRjSMm", "11671830NSHzpT", "81894iLDHcx", "75850vyxJsF", "25808255zYDyDV", " API error", "exports", "replace", "243190jAxVTw", "ytdown", "4012375iJlxDr", "143864BjUflq", "1100LwzRcr", "alldown", "3AOEPKP", "24yyLnqk", "10478290WtWHWc", "828898wdthdD", "2376584KSgWuY", "13BTHYkB", "instagram", "108HgYBxx", "22970FXpPfg", "2200cRqrvk", "2252259XMVQLV", "twitterdown", "2sqtsJt", "1788900OPZUAI", "shift", "likee", "https://nayan-video-downloader.vercel.app/", "data", "tikdown", "MAHABUB RAHMAN", "6JefULS", "537102YRpfrk", "GDLink", "pintarest", "20PMZLAv", "ndown", "54693qrAIPr", "wa.me/+8801613356376", "891084jJazVa", "5691952pFkHfU", "564403uKRRPU", "toUpperCase"];
+  _0x31f4 = function () {
+    return _0x1adb1c;
+  };
+  return _0x31f4();
+}
+const _0x4515af = _0x1e02;
+(function (_0x9c35fc, _0x3b68dd) {
+  const _0x531094 = _0x6590, _0xb7a554 = _0x1e02, _0x4586d2 = _0x9c35fc();
+  while (true) {
+    try {
+      const _0x5b8840 = parseInt(_0xb7a554(423)) / 1 + -parseInt(_0xb7a554(385)) / 2 * (-parseInt(_0xb7a554(391)) / 3) + -parseInt(_0xb7a554(421)) / 4 * (-parseInt(_0xb7a554(412)) / 5) + parseInt(_0xb7a554(417)) / 6 * (-parseInt(_0xb7a554(414)) / 7) + -parseInt(_0xb7a554(405)) / 8 * (-parseInt(_0xb7a554(387)) / 9) + parseInt(_0xb7a554(407)) / 10 + -parseInt(_0xb7a554(396)) / 11;
+      if (_0x5b8840 === _0x3b68dd) break; else _0x4586d2[_0x531094(423)](_0x4586d2[_0x531094(404)]());
+    } catch (_0x13799d) {
+      _0x4586d2.push(_0x4586d2[_0x531094(404)]());
+    }
+  }
+}(_0x43d1, 703982));
+const _0x15f4b6 = _0x1272;
+(function (_0x3c986, _0x3d525d) {
+  const _0x22077e = _0x6590, _0x51393e = _0x1e02, _0x494c44 = _0x1272, _0x20ae90 = _0x3c986();
+  while (true) {
+    try {
+      const _0x3516d8 = -parseInt(_0x494c44(380)) / 1 * (parseInt(_0x494c44(392)) / 2) + -parseInt(_0x494c44(389)) / 3 + -parseInt(_0x494c44(405)) / 4 * (parseInt(_0x494c44(404)) / 5) + parseInt(_0x494c44(381)) / 6 + parseInt(_0x494c44(395)) / 7 + -parseInt(_0x494c44(396)) / 8 + parseInt(_0x494c44(400)) / 9 * (parseInt(_0x494c44(386)) / 10);
+      if (_0x3516d8 === _0x3d525d) break; else _0x20ae90[_0x51393e(415)](_0x20ae90.shift());
+    } catch (_0x52146f) {
+      _0x20ae90[_0x22077e(423)](_0x20ae90[_0x51393e(418)]());
+    }
+  }
+}(_0x41a4, 286567));
+function _0x1e02(_0x259cfd, _0x486f55) {
+  const _0x3ef652 = _0x43d1();
+  return _0x1e02 = function (_0xbd1314, _0x35de52) {
+    _0xbd1314 = _0xbd1314 - 384;
+    let _0x31dd77 = _0x3ef652[_0xbd1314];
+    return _0x31dd77;
+  }, _0x1e02(_0x259cfd, _0x486f55);
+}
+const axios = require(_0x15f4b6(383)), apiBaseUrl = _0x4515af(422);
+function _0x41a4() {
+  const _0x6d3c92 = _0x6590, _0x3c323b = _0x4515af, _0x1410a6 = [_0x6d3c92(429), _0x3c323b(395), _0x3c323b(402), _0x3c323b(400), _0x3c323b(416), _0x3c323b(399), _0x3c323b(419), _0x3c323b(409), "axios", _0x6d3c92(417), _0x3c323b(388), _0x3c323b(393), _0x3c323b(410), _0x3c323b(397), "637905bRCLyk", _0x3c323b(404), _0x3c323b(413), _0x3c323b(384), _0x3c323b(392), _0x6d3c92(415), _0x6d3c92(441), _0x6d3c92(442), _0x3c323b(406), _0x3c323b(401), _0x3c323b(398), _0x3c323b(408)];
+  return _0x41a4 = function () {
+    return _0x1410a6;
+  }, _0x41a4();
+}
+module[_0x15f4b6(388)] = {ndown: createRequest(_0x15f4b6(394)), instagram: createRequest(_0x4515af(403)), tikdown: createRequest(_0x4515af(389)), ytdown: createRequest(_0x4515af(411)), threads: createRequest(_0x15f4b6(398)), twitterdown: createRequest(_0x15f4b6(382)), fbdown2: createRequest("fbdown2", (_0x285e5e, _0x17c477) => ({url: _0x285e5e, key: _0x17c477})), GDLink: createRequest(_0x15f4b6(385)), pintarest: createRequest(_0x4515af(390)), capcut: createRequest(_0x15f4b6(402)), likee: createRequest(_0x15f4b6(399)), alldown: createRequest(_0x4515af(386))};
+function _0x1272(_0x436910, _0x27210d) {
+  const _0x37aa76 = _0x41a4();
+  return _0x1272 = function (_0x3ac6fd, _0x27a46e) {
+    _0x3ac6fd = _0x3ac6fd - 380;
+    let _0x2ef1ea = _0x37aa76[_0x3ac6fd];
+    return _0x2ef1ea;
+  }, _0x1272(_0x436910, _0x27210d);
+}
+function createRequest(_0x21f814, _0x1c361e) {
+  return (_0x43947a, _0x24db13) => {
+    return new Promise(async _0x14b5d4 => {
+      const _0x4d740f = _0x1e02, _0x851753 = _0x1272;
+      try {
+        const _0x89f47f = _0x1c361e ? _0x1c361e(_0x43947a, _0x24db13) : {url: _0x43947a}, _0xfbb468 = await axios[_0x851753(391)]("" + apiBaseUrl + _0x21f814, {params: _0x89f47f});
+        _0x14b5d4(_0xfbb468[_0x4d740f(394)]);
+      } catch (_0x3be8d7) {
+        _0x14b5d4({developer: _0x851753(393), devfb: _0x851753(397), devwp: _0x851753(384), status: true, msg: capitalize(_0x21f814[_0x851753(390)](/^\w/, _0x598fcc => _0x598fcc[_0x851753(387)]())) + _0x851753(401)});
+      }
+    });
+  };
+}
+function capitalize(_0x2536aa) {
+  const _0x35d4bb = _0x4515af, _0x29ff6e = _0x15f4b6;
+  return _0x2536aa[_0x29ff6e(403)](0)[_0x29ff6e(387)]() + _0x2536aa[_0x35d4bb(420)](1);
+}
